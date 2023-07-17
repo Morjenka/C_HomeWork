8232224708f332b171c7b5e22e00da07241215c3
Marina <morjenka-z@mail.ru>
2023-07-18 01:04:12 +0300

    Error, при наборе 22, 3 и 9

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает 
+// на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+if(max > numberB)
+    {
+        max = numberA;
+    }
+else
+max = numberB;
+
+        if(max > numberC)
+            {
+                max = numberA;
+            }
+    else
+    max = numberC;
+
+if(max > numberC)
+    {
+        max = numberB;
+    }
+else
+max = numberC;
+Console.WriteLine("max = " + max);
+
